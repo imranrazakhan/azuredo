5ef30bbbfe3cd3b84e2e3f180c484c5c7f60656e
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-04-22 16:57:59 +0200

    Create RG and K8S Cluster

---
+++ main/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.49.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "imranrg-${var.env_name}"
+  location = "West US 2"
+  tags     = {
+         "Environment" = "Terraform Getting Started"
+         "Team"        = "DevOps"
+        }
+
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "${var.cluster_name}-${var.env_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "imrank8scluster-${var.env_name}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "1"
+    vm_size    = var.instance_type
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
