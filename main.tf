ed1ecf9cf1affc4c17ffaa494c9377218d5c3f33
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-05-03 20:02:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.49.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg-dev"
+    storage_account_name = "<storage-account>"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 module "dev_cluster" {
     source       = "./main"
     env_name     = "dev"
     cluster_name = "imrank8scluster"
 }
+
