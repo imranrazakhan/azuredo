79ba5ed7cc7f721bb53849c38d3068ebdd2e5953
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-05-04 23:13:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,9 +19,40 @@ provider "azurerm" {
 }
 
 
-module "dev_cluster" {
-    source       = "./main"
-    env_name     = "dev"
-    cluster_name = "imrank8scluster"
+resource "azurerm_resource_group" "rg" {
+  name     = "imranrg-${var.env_name}"
+  location = "West US 2"
+  tags     = {
+         "Environment" = "Terraform Getting Started"
+         "Team"        = "DevOps"
+        }
+
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acr${var.env_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "${var.cluster_name}-${var.env_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "imrank8scluster-${var.env_name}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "1"
+    vm_size    = var.instance_type
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 
