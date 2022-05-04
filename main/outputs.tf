0ac4a9f765650f3b2e383b31bda9cbcf83efa4b8
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-05-04 23:14:48 +0200

    Delete main directory

--- main/outputs.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "kubeconfig" {
-  depends_on   = [azurerm_kubernetes_cluster.cluster]
-  filename     = "kubeconfig-${var.env_name}"
-  content      = azurerm_kubernetes_cluster.cluster.kube_config_raw
-}
